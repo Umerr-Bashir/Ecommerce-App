f385e732d335347242cfb5d0a957f3c30b9536b5
MUHAMMAD AHMED <ahmed.asif4849@gmail.com>
2025-10-28 21:43:48 +0500

    address dto
    ADDRESS DTO

---
+++ EcommerceApp/DTOs/AddressDTO/AddressCreateDTO.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApp.DTOs.AddressDTO
+{
+    public class AddressCreateDTO
+    {
+        [Required(ErrorMessage = "CustomerId is required.")]
+        public int CustomerId { get; set; }
+
+
+        [Required(ErrorMessage = "Present Address is required.")]
+        [StringLength(40, ErrorMessage = "Present Address cannot exceed 100 characters.")]
+        public string PresentAddress { get; set; }
+
+
+        [Required(ErrorMessage = "Permanent Address is required.")]
+        [StringLength(40, ErrorMessage = "Permanent Address cannot exceed 100 characters.")]
+        public string PermanentAddress { get; set; }
+
+
+        [Required(ErrorMessage = "State is required.")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "State is required")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "City is required")]
+        public string City { get; set; }
+
+
+        [Required(ErrorMessage = "Postal code is required.")]
+        [StringLength(8, MinimumLength = 6, ErrorMessage = "Postal code is required")]
+        public string PostalCode { get; set; }
+
+
+
+    }
+}
